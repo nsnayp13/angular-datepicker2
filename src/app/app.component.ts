b6601ece007659bebc8f03b5f9751245d2163281
Ivanov.n/work <ivanov@etsgroup.ru>
2020-02-17 16:15:15 +0300

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit {
   title = "angular-datepicker";
   date: Date;
   selectedDates: Date[];
-  days: [];
+  days: any[];
 
   ngOnInit(): void {
     this.selectedDates = [
