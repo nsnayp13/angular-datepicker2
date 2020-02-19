9bb460b26b4b69fc17d06d397540692ba02c0b82
Ivanov.n/work <ivanov@etsgroup.ru>
2020-02-19 12:39:56 +0300

    fke test

---
+++ projects/angular-datepicker2/src/lib/day-view/day-view.component.spec.ts
@@ -0,0 +1,65 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { DayViewComponent } from './day-view.component';
+import { DayService } from '../_service/day.service';
+import { CalendarService } from '../_service/calendar.service';
+import { of, BehaviorSubject } from 'rxjs';
+describe("DayViewComponent", () => {
+    let component: DayViewComponent;
+    let fixture: ComponentFixture<DayViewComponent>;
+    let dayService;
+    let calendarService;
+
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                DayViewComponent,
+            ],
+            imports: [],
+            providers: []
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DayViewComponent);
+        component = fixture.componentInstance;
+        dayService = fixture.debugElement.injector.get(DayService);
+        calendarService = fixture.debugElement.injector.get(CalendarService);
+
+        // spyOnAllFunctions(calendarService);
+        spyOn(calendarService.selectedDates, 'subscribe').and.returnValue([new Date()]);
+
+
+        spyOn(dayService, 'createDay').and.returnValue({
+            isDisabled: false,
+            isHovered: false,
+            isSelected: false,
+            isWeekEnd: false,
+            date: new Date(),
+            title: '',
+            template: ''
+        });
+
+
+
+
+        component.date = new Date();
+        component.thisMonth = true;
+        calendarService.selectedDates.next([new Date()])
+
+
+        fixture.detectChanges();
+    });
+
+    it("should have a defined component", () => {
+
+
+
+
+        fixture.detectChanges();
+
+
+        expect(true).toBeTruthy();
+    });
+
+});
