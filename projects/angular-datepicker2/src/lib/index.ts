1c7013f3b6d773b5de5cbe150ce68e7606bdcada
Ivanov.n/work <ivanov@etsgroup.ru>
2021-09-09 15:32:23 +0300

    Revert "test"
    This reverts commit 41ed62861516cf62b9da2169f69b7bc1ddd9a00b.

--- projects/angular-datepicker2/src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "../lib/calendar/angular-datepicker2.component";
-export * from "../lib/angular-datepicker2.module";
-export * from "../lib/interfaces";
-export * from "../lib/day.directive";
