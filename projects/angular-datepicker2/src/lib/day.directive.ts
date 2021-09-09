b6c799b38c0242a7e59934f30d1567a398e11e65
Ivanov.n/work <ivanov@etsgroup.ru>
2021-09-09 15:37:32 +0300

    test

--- projects/angular-datepicker2/src/lib/day.directive.ts
+++ projects/angular-datepicker2/src/lib/day.directive.ts
@@ -1,10 +1,19 @@
-import { ViewContainerRef, Directive, Input, TemplateRef } from "@angular/core";
+import {
+  ViewContainerRef,
+  Directive,
+  Input,
+  TemplateRef,
+  AfterContentInit,
+  AfterViewInit,
+} from "@angular/core";
 
 @Directive({
   selector: "[day]",
+  //providers: [TemplateRef],
 })
-export class DayDirective {
+export class DayDirective implements AfterContentInit, AfterViewInit {
   context: any | null = null;
+  __created = false;
 
   constructor(
     private template: TemplateRef<any>,
@@ -18,11 +27,27 @@ export class DayDirective {
       $implicit: this.date,
     };
   }
+
+  __create() {
+    if (!this.__created && this.template) {
+      this.container.createEmbeddedView(this.template, this.context);
+      this.__created = true;
+    }
+  }
+
   ngOnInit() {
     this.context = {
       $implicit: this.date,
       date: this.date,
     };
-    this.container.createEmbeddedView(this.template, this.context);
+    this.__create();
+  }
+
+  ngAfterContentInit() {
+    this.__create();
+  }
+
+  ngAfterViewInit() {
+    this.__create();
   }
 }
