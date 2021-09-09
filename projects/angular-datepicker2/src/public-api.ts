857834ab96c94d6d3cdcf49dbd7da25df88f3852
Ivanov.n/work <ivanov@etsgroup.ru>
2021-09-09 11:27:28 +0300

    fix: public api dayDirective

--- projects/angular-datepicker2/src/public-api.ts
+++ projects/angular-datepicker2/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of angular-datepicker2
  */
 
-export * from './lib/calendar/angular-datepicker2.component';
-export * from './lib/angular-datepicker2.module';
-export * from './lib/interfaces';
+export * from "./lib/calendar/angular-datepicker2.component";
+export * from "./lib/angular-datepicker2.module";
+export * from "./lib/interfaces";
+export * from "./lib/day.directive";
